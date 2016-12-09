21afdb5aeeb432c85bbd3f2bbdeacb4bfe941e57
zhangkl <taitoubiyan1@gmail.com>
2016-12-09 17:58:58 +0800

    fight

---
+++ include/fight.hrl
@@ -0,0 +1,33 @@
+-ifndef(FIGHT_HRL).
+-define(FIGHT_HRL, true).
+
+-define(DUTY_DAOZEI, 1).    %%盗贼
+-define(DUTY_QIUBITE, 2).   %%丘比特
+-define(DUTY_HUNXUEER, 3).  %%混血儿
+-define(DUTY_SHOUWEI, 4).   %%守卫
+-define(DUTY_LANGREN, 5).   %%狼人
+-define(DUTY_NVWU, 6).      %%女巫
+-define(DUTY_YUYANJIA, 7).  %%预言家
+-define(DUTY_PINGMIN, 8).   %%平民
+
+-define(DUTY_TYPE_SPECIAL, 1).  %%特殊身份
+-define(DUTY_TYPE_SHENMIN, 2).  %%神民
+
+-define(DUTY_LIST_SPECIAL, [?DUTY_DAOZEI, ?DUTY_QIUBITE,?DUTY_HUNXUEER, ?DUTY_SHOUWEI]).
+-define(DUTY_LIST_SHENMIN, [?DUTY_NVWU, ?DUTY_YUYANJIA]).
+
+-define(TURN_UP, 0).
+-define(TURN_DOWN, 1).
+
+-define(MFIGHT, #{room_id => 0,
+                  player_list => [],    %% #{seat_id, player_id}
+                  offline_list => [],   %% seat_id
+                  out_player_list => [],%% 出局列表 seat_id
+                  duty_list => [],      %% #{seat_id, 职责}
+                  left_op_list => [],   %% 剩余操作seat_id 按照顺序排好
+                  op => 0,              %% 当前进行的操作
+                  last_op_data => #{}   %% 上一轮操作的数据, 杀了几号, 投了几号等等
+                  }).
+
+-endif.
+
---
+++ src/srv/fight_srv.erl
@@ -0,0 +1,165 @@
+%% @author zhangkl
+%% @doc fight_srv.
+%% 2016
+
+-module(gen_fsm_module).
+-behaviour(gen_fsm).
+-export([init/1, state_name/2, state_name/3, handle_event/3, handle_sync_event/4, handle_info/3, terminate/3, code_change/4]).
+
+%% ====================================================================
+%% API functions
+%% ====================================================================
+-export([]).
+
+
+
+%% ====================================================================
+%% Behavioural functions
+%% ====================================================================
+-record(state, {}).
+
+%% init/1
+%% ====================================================================
+%% @doc <a href="http://www.erlang.org/doc/man/gen_fsm.html#Module:init-1">gen_fsm:init/1</a>
+-spec init(Args :: term()) -> Result when
+    Result :: {ok, StateName, StateData}
+            | {ok, StateName, StateData, Timeout}
+            | {ok, StateName, StateData, hibernate}
+            | {stop, Reason}
+            | ignore,
+    StateName :: atom(),
+    StateData :: term(),
+    Timeout :: non_neg_integer() | infinity,
+    Reason :: term().
+%% ====================================================================
+init([]) ->
+    {ok, state_name, #state{}}.
+
+
+%% state_name/2
+%% ====================================================================
+%% @doc <a href="http://www.erlang.org/doc/man/gen_fsm.html#Module:StateName-2">gen_fsm:StateName/2</a>
+-spec state_name(Event :: timeout | term(), StateData :: term()) -> Result when
+    Result :: {next_state, NextStateName, NewStateData}
+            | {next_state, NextStateName, NewStateData, Timeout}
+            | {next_state, NextStateName, NewStateData, hibernate}
+            | {stop, Reason, NewStateData},
+    NextStateName :: atom(),
+    NewStateData :: term(),
+    Timeout :: non_neg_integer() | infinity,
+    Reason :: term().
+%% ====================================================================
+% @todo implement actual state
+state_name(Event, StateData) ->
+    {next_state, state_name, StateData}.
+
+
+%% state_name/3
+%% ====================================================================
+%% @doc <a href="http://www.erlang.org/doc/man/gen_fsm.html#Module:StateName-3">gen_fsm:StateName/3</a>
+-spec state_name(Event :: term(), From :: {pid(), Tag :: term()}, StateData :: term()) -> Result when
+    Result :: {reply, Reply, NextStateName, NewStateData}
+            | {reply, Reply, NextStateName, NewStateData, Timeout}
+            | {reply, Reply, NextStateName, NewStateData, hibernate}
+            | {next_state, NextStateName, NewStateData}
+            | {next_state, NextStateName, NewStateData, Timeout}
+            | {next_state, NextStateName, NewStateData, hibernate}
+            | {stop, Reason, Reply, NewStateData}
+            | {stop, Reason, NewStateData},
+    Reply :: term(),
+    NextStateName :: atom(),
+    NewStateData :: atom(),
+    Timeout :: non_neg_integer() | infinity,
+    Reason :: normal | term().
+%% ====================================================================
+state_name(Event, From, StateData) ->
+    Reply = ok,
+    {reply, Reply, state_name, StateData}.
+
+
+%% handle_event/3
+%% ====================================================================
+%% @doc <a href="http://www.erlang.org/doc/man/gen_fsm.html#Module:handle_event-3">gen_fsm:handle_event/3</a>
+-spec handle_event(Event :: term(), StateName :: atom(), StateData :: term()) -> Result when
+    Result :: {next_state, NextStateName, NewStateData}
+            | {next_state, NextStateName, NewStateData, Timeout}
+            | {next_state, NextStateName, NewStateData, hibernate}
+            | {stop, Reason, NewStateData},
+    NextStateName :: atom(),
+    NewStateData :: term(),
+    Timeout :: non_neg_integer() | infinity,
+    Reason :: term().
+%% ====================================================================
+handle_event(Event, StateName, StateData) ->
+    {next_state, StateName, StateData}.
+
+
+%% handle_sync_event/4
+%% ====================================================================
+%% @doc <a href="http://www.erlang.org/doc/man/gen_fsm.html#Module:handle_sync_event-4">gen_fsm:handle_sync_event/4</a>
+-spec handle_sync_event(Event :: term(), From :: {pid(), Tag :: term()}, StateName :: atom(), StateData :: term()) -> Result when
+    Result :: {reply, Reply, NextStateName, NewStateData}
+            | {reply, Reply, NextStateName, NewStateData, Timeout}
+            | {reply, Reply, NextStateName, NewStateData, hibernate}
+            | {next_state, NextStateName, NewStateData}
+            | {next_state, NextStateName, NewStateData, Timeout}
+            | {next_state, NextStateName, NewStateData, hibernate}
+            | {stop, Reason, Reply, NewStateData}
+            | {stop, Reason, NewStateData},
+    Reply :: term(),
+    NextStateName :: atom(),
+    NewStateData :: term(),
+    Timeout :: non_neg_integer() | infinity,
+    Reason :: term().
+%% ====================================================================
+handle_sync_event(Event, From, StateName, StateData) ->
+    Reply = ok,
+    {reply, Reply, StateName, StateData}.
+
+
+%% handle_info/3
+%% ====================================================================
+%% @doc <a href="http://www.erlang.org/doc/man/gen_fsm.html#Module:handle_info-3">gen_fsm:handle_info/3</a>
+-spec handle_info(Info :: term(), StateName :: atom(), StateData :: term()) -> Result when
+    Result :: {next_state, NextStateName, NewStateData}
+            | {next_state, NextStateName, NewStateData, Timeout}
+            | {next_state, NextStateName, NewStateData, hibernate}
+            | {stop, Reason, NewStateData},
+    NextStateName :: atom(),
+    NewStateData :: term(),
+    Timeout :: non_neg_integer() | infinity,
+    Reason :: normal | term().
+%% ====================================================================
+handle_info(Info, StateName, StateData) ->
+    {next_state, StateName, StateData}.
+
+
+%% terminate/3
+%% ====================================================================
+%% @doc <a href="http://www.erlang.org/doc/man/gen_fsm.html#Module:terminate-3">gen_fsm:terminate/3</a>
+-spec terminate(Reason, StateName :: atom(), StateData :: term()) -> Result :: term() when
+    Reason :: normal
+            | shutdown
+            | {shutdown, term()}
+            | term().
+%% ====================================================================
+terminate(Reason, StateName, StatData) ->
+    ok.
+
+
+%% code_change/4
+%% ====================================================================
+%% @doc <a href="http://www.erlang.org/doc/man/gen_fsm.html#Module:code_change-4">gen_fsm:code_change/4</a>
+-spec code_change(OldVsn, StateName :: atom(), StateData :: term(), Extra :: term()) -> {ok, NextStateName :: atom(), NewStateData :: term()} when
+    OldVsn :: Vsn | {down, Vsn},
+    Vsn :: term().
+%% ====================================================================
+code_change(OldVsn, StateName, StateData, Extra) ->
+    {ok, StateName, StateData}.
+
+
+%% ====================================================================
+%% Internal functions
+%% ====================================================================
+
+
