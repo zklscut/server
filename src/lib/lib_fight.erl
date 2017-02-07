59a13d2be4ec4af08ebf55b584388c3e73d26a84
huanggang703797130 <703797130@qq.com>
2017-02-07 23:10:05 +0800

    退水需要退出发言

--- src/lib/lib_fight.erl
+++ src/lib/lib_fight.erl
@@ -495,7 +495,9 @@ do_skill_inner(_SeatId, ?OP_SKILL_CHANGE_JINGZHANG, [SelectId], State) ->
 
 do_skill_inner(SeatId, ?OP_SKILL_EIXT_PART_JINGZHANG, [_SelectId], State) ->
     NewState = maps:put(exit_jingzhang, maps:get(exit_jingzhang, State) ++ [SeatId], State),
-    maps:put(part_jingzhang, maps:get(part_jingzhang, State) -- [SeatId], NewState).
+    StateAfterJingZhang = maps:put(part_jingzhang, maps:get(part_jingzhang, State) -- [SeatId], NewState),
+    StateAfterFayanTurn = maps:put(fayan_turn, maps:get(fayan_turn, State) -- [SeatId], StateAfterJingZhang),
+    StateAfterFayanTurn.
 
 rand_in_alive_seat(State) ->
     util:rand_in_list(get_alive_seat_list(State)).
