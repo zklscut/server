52fb69468ea66259072e8b0a084c586f35a49b84
huanggang703797130 <703797130@qq.com>
2017-03-07 09:57:21 +0800

    11

--- src/srv/fight_srv.erl
+++ src/srv/fight_srv.erl
@@ -1358,6 +1358,7 @@ notice_player_op(Op, AttachData, SeatList, State) ->
         true->
             State;
         _->
+            cancel_fight_fsm_event_timer(?TIMER_TIMEOUT),
             start_fight_fsm_event_timer(?TIMER_TIMEOUT, UseWaitTime),
             StateAfterAttackData = maps:put(wait_op_attack_data, WaitTime, State),
             %%正常延时时间
