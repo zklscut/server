442b9b2de3a720a800c55366dfd9d89f4629a50c
huanggang703797130 <703797130@qq.com>
2017-03-11 14:52:43 +0800

    11

--- src/mod/mod_fight.erl
+++ src/mod/mod_fight.erl
@@ -15,18 +15,18 @@
 
 notice_op(#m__fight__notice_op__l2s{op = Op,
 									op_list = OpList}, Player) ->
-	fight_srv:player_op(lib_room:get_fight_pid_by_player(Player), 
+	fight_srv:player_op(lib_player:get_fight_pid(Player), 
 		lib_player:get_player_id(Player), Op, OpList),
 	{ok, Player}.
 
 speak(#m__fight__speak__l2s{chat = PChat}, Player) ->
-    fight_srv:player_speak(lib_room:get_fight_pid_by_player(Player), 
+    fight_srv:player_speak(lib_player:get_fight_pid(Player), 
         lib_player:get_player_id(Player), PChat),
     {ok, Player}.
 
 do_skill(#m__fight__do_skill__l2s{op = Op,
                                   op_list = OpList}, Player) ->
-    fight_srv:player_skill(lib_room:get_fight_pid_by_player(Player), 
+    fight_srv:player_skill(lib_player:get_fight_pid(Player), 
         lib_player:get_player_id(Player), Op, OpList),
     {ok, Player}.
 
