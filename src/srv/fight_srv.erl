37b758ef27584bfe7bfae59d1762475f14c75bc6
huanggang703797130 <703797130@qq.com>
2017-03-16 15:43:59 +0800

    11

--- src/srv/fight_srv.erl
+++ src/srv/fight_srv.erl
@@ -163,8 +163,9 @@ state_select_card({player_op, PlayerId, ?OP_SELECT_DUTY, [Duty]}, State)->
             %%提示已经操作过
             lib_fight:send_to_seat(#m__fight__select_duty__s2l{result = 1}, SeatId, State),
             State;
-        _->
-            case DiamondNun < ?SELECT_DUTY_CAST of
+        _-> 
+            DiamondNum = mod_resource:get_num(?RESOURCE_DIAMOND, PlayerId),
+            case DiamondNum < ?SELECT_DUTY_CAST of
                 true->
                     %%提示金币不够
                     lib_fight:send_to_seat(#m__fight__select_duty__s2l{result = 2}, SeatId, State),
