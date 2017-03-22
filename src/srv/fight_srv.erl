a52c1858587e46d08d45eab1b4a93dd98979e3a2
huanggang703797130 <703797130@qq.com>
2017-03-22 11:32:53 +0800

    11

--- src/srv/fight_srv.erl
+++ src/srv/fight_srv.erl
@@ -1195,7 +1195,8 @@ handle_event({skill, PlayerId, Op, OpList}, StateName, State) ->
     end;
 
 handle_event({player_chat, Chat, PlayerId}, StateName, State)->
-    lib_fight:do_send_fayan(PlayerId, Chat, State).
+    lib_fight:do_send_fayan(PlayerId, Chat, State),
+    {next_state, StateName, State} .
 
 handle_event({player_online, PlayerId}, StateName, State) ->
     OfflineList = maps:get(offline_list, State),
