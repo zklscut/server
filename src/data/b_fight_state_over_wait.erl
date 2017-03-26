ffa6d82951465b648ab61aeb7ca60780e2e2b0c0
huanggang703797130 <703797130@qq.com>
2017-03-26 17:32:40 +0800

    11

--- src/data/b_fight_state_over_wait.erl
+++ src/data/b_fight_state_over_wait.erl
@@ -1,12 +1,12 @@
 -module(b_fight_state_over_wait).
 -export([get/1]).
 get(state_toupiao)->
-	6000;
+	0;
 get(state_toupiao_mvp)->
-	6000;
+	0;
 get(state_toupiao_carry)->
-	6000;
+	0;
 get(state_yuyanjia)->
-	6000;
+	0;
 get(_) ->
-    15000.
+    0.
