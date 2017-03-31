2d9f8633060d055fd227fc493b2168ac10d49cef
huanggang703797130 <703797130@qq.com>
2017-03-31 23:01:20 +0800

    11

--- include/resource.hrl
+++ include/resource.hrl
@@ -8,10 +8,10 @@
 -define(RESOURCE_EXP, 4).
 -define(RESOURCE_LV, 5).
 
--define(RESOURCE_MVP, 5).
--define(RESOURCE_CARRY, 5).
--define(RESOURCE_FIGHTING, 5).
--define(RESOURCE_RANK_SCORE, 5).
+-define(RESOURCE_MVP, 6).
+-define(RESOURCE_CARRY, 7).
+-define(RESOURCE_FIGHTING, 8).
+-define(RESOURCE_RANK_SCORE, 9).
 
 -define(RESOURCE_MAX_LV, 30).
 
