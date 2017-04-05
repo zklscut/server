8c6e2d3b9aa48296a986729eed78eacb1fdc34dc
huanggang703797130 <703797130@qq.com>
2017-04-05 23:46:00 +0800

    11

--- include/player.hrl
+++ include/player.hrl
@@ -1,7 +1,7 @@
 -ifndef(PLAYER_HRL).
 -define(PLAYER_HRL, true).
 
--define(PLAYER_DATA, #{resource => #{},
+-define(PLAYER_DATA, #{resource => #{9=>1200},
                        win_rate_list => #{},
                        head_data => <<>>,
                        change_name_cnt => 0}).
